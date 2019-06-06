2f88f1bf9e8f33bb08bc25855ebc0a96dd668897
arigatopix <teeruch.k@gmail.com>
2019-06-06 15:44:20 +0700

    :fire: views look index.html

--- leadmanager_project/frontend/views.py
+++ leadmanager_project/frontend/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    ''' ให้ดูที่ index.html ซึ่งเป็น react templates'''
+    return render(request, 'frontend/index.html')
